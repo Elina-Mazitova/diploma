a4e4fda75f066d1496f99c85366e0c71412a6ce9
Elina Mazitova <elinamazitova14@gmail.com>
2026-02-15 17:45:06 +0300

    refactor utils

--- ui/conftest.py
+++ ui/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from utils.browser import start_browser, stop_browser
+from ui.utils.browser import start_browser, stop_browser
 from pages.login_page import LoginPage
 from pages.main_page import MainPage
 from ui.data.credentials import TODOIST_LOGIN, TODOIST_PASSWORD
