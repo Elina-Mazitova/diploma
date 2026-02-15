26360dc426103c6d9136b7ea1d08e038dbcf4fc4
Elina Mazitova <elinamazitova14@gmail.com>
2026-02-15 18:12:38 +0300

    conftest.py

--- ui/conftest.py
+++ ui/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from ui.utils.browser import start_browser, stop_browser
 from ui.pages.login_page import LoginPage
 from ui.pages.main_page import MainPage
-from ui.data.credentials import TODOIST_LOGIN, TODOIST_PASSWORD
+from ui.data.credentials import TODOIST_LOGIN, TODOIST_PASSWORD1
 
 
 @pytest.fixture(scope='function', autouse=True)
