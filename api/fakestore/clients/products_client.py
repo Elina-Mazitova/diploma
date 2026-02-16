53116587ae1965f4efd12ae86f8be46c39f50540
Elina Mazitova <elinamazitova14@gmail.com>
2026-02-16 22:37:06 +0300

    add products_client.py

---
+++ api/fakestore/clients/products_client.py
@@ -0,0 +1,19 @@
+from .base_client import BaseClient
+
+
+class ProductsClient(BaseClient):
+
+    def get_products(self):
+        return self.get("/products")
+
+    def get_product_by_id(self, product_id: int):
+        return self.get(f"/products/{product_id}")
+
+    def create_product(self, body: dict):
+        return self.post("/products", json=body)
+
+    def update_product(self, product_id: int, body: dict):
+        return self.put(f"/products/{product_id}", json=body)
+
+    def delete_product(self, product_id: int):
+        return self.delete(f"/products/{product_id}")
