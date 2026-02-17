645b26fceead224782e3d7d6225d5ae51078fad2
Elina Mazitova <elinamazitova14@gmail.com>
2026-02-17 19:32:59 +0300

    add demoqa to conftest.py

--- api/conftest.py
+++ api/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from api.fakestore.clients.products_client import ProductsClient
-from api.fakestore_auth.clients.auth_client import AuthClient
+from api.demoqa.clients.auth_client import AuthClient
+from selene import browser
 
 @pytest.fixture
 def base_url():
@@ -11,7 +12,14 @@ def base_url():
 def products_client(base_url):
     return ProductsClient(base_url=base_url)
 
+
 @pytest.fixture
 def auth_client():
-    return AuthClient()
+    return AuthClient(base_url="https://demoqa.com")
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_browser():
+    browser.config.timeout = 10
+    browser.config.window_width = 1400
+    browser.config.window_height = 900
 
