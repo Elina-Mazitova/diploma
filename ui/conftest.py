3893f97eb1a0678b736ee6947b0cd045143cd70d
Elina Mazitova <elinamazitova14@gmail.com>
2026-02-21 20:57:48 +0300

    refactored conftest.py

--- ui/conftest.py
+++ ui/conftest.py
@@ -1,11 +1,12 @@
 import pytest
-from ui.utils.browser import start_browser, stop_browser
+
+from ui.data.credentials import TODOIST_LOGIN, TODOIST_PASSWORD
 from ui.pages.login_page import LoginPage
 from ui.pages.main_page import MainPage
-from ui.data.credentials import TODOIST_LOGIN, TODOIST_PASSWORD
+from ui.utils.browser import start_browser, stop_browser
 
 
-@pytest.fixture(scope='function', autouse=True)
+@pytest.fixture(scope="function", autouse=True)
 def browser_management():
     start_browser()
     yield
@@ -17,6 +18,6 @@ def authorized_user():
     login_page = LoginPage()
     main_page = MainPage()
 
-    login_page.open_login_page().login(TODOIST_LOGIN, TODOIST_PASSWORD)
+    login_page.open().login(TODOIST_LOGIN, TODOIST_PASSWORD)
 
     return main_page
