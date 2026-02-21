11453a36242353cde22d5ecff0acaa860a22f814
Elina Mazitova <elinamazitova14@gmail.com>
2026-02-22 00:15:15 +0300

    changed .env urls

--- ui/conftest.py
+++ ui/conftest.py
@@ -1,9 +1,17 @@
 import pytest
+import os
+from dotenv import load_dotenv
+from selene import browser
 
 from ui.data.credentials import TODOIST_LOGIN, TODOIST_PASSWORD
 from ui.pages.login_page import LoginPage
 from ui.pages.main_page import MainPage
 from ui.utils.browser import start_browser, stop_browser
+
+
+load_dotenv()
+
+browser.config.base_url = os.getenv("UI_BASE_URL")
 
 
 @pytest.fixture(scope="function", autouse=True)
